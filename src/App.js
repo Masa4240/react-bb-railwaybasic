30c5a4aced3164f3c3e2a542cc5228e1090d8a27
Masa4240 <zeromirror.m.n.0@gmail.com>
2022-11-05 11:49:17 +0900

    Initial commit for station 3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 //import logo from './logo.svg';
 import './App.css';
+import { CreateThread } from './CreateThread';
 import { Header } from './Header';
 import { ThreadList } from './ThreadList';
 import { GetThreadList } from './ThreadList';
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header/>
+      <CreateThread />
       {/* <button onClick={GetThreadList}>push here</button> */}
       <ThreadList />
     </div>
---
+++ src/CreateThread.js
@@ -0,0 +1,40 @@
+import * as React from 'react'
+export const CreateThread = () => {
+    const [NewThread, setNewThread] = React.useState("");
+    let doClick = () => {
+      console.log("Clicked")
+      const url = "https://2y6i6tqn41.execute-api.ap-northeast-1.amazonaws.com/threads"
+      const requestOptions ={
+        method: 'POST',
+        mode: 'no-cors',
+        headers:{'Content-Type': 'application/json'},
+        body: JSON.stringify({title: NewThread})
+      };
+    fetch(url,requestOptions)
+    setNewThread("")
+    }
+
+    return <div>
+        <input type="text" value={NewThread}
+        onChange={(event) => setNewThread(event.target.value)}/>
+      <p>{NewThread}</p>
+      {/* <input
+          type="submit"
+          value="add"
+          onSubmit={CreateNewThread(NewThread)}
+          /> */}
+    <button onClick={doClick} >Create New Thread</button>
+            </div>
+}
+
+function CreateNewThread (string){
+    const url = "https://2y6i6tqn41.execute-api.ap-northeast-1.amazonaws.com/threads"
+    console.log("Clicked")
+    console.log(string)
+    const requestOptions ={
+        method: 'POST',
+        headers:{'Content-Type': 'application/json'},
+        body: JSON.stringify({title: string})
+      };
+    //fetch(url,requestOptions)
+}
